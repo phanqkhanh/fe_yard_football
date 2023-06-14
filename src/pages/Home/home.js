9162b16bb31581ba9818ce978207d2baaf1b51bc
phanqkhanh <phanquockhanh9196@gmail.com>
2023-06-14 21:57:23 +0700

    check deploy

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <p>Trang chủ {process.env.REACT_APP_BASE_URL}</p>
+            <p>Trang chủ update{process.env.REACT_APP_BASE_URL}</p>
             <button
                 onClick={() => {
                     console.log(123);
